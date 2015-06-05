60505d87817253279f0d3bf8ee2828c69eb452fd
Daniel Nilsson <daniel.nilsson.9@gmail.com>
2015-06-05 17:39:53 +0200

    prepare for android studio switch

--- demo/gen/afzkl/development/colorpickerview/demo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package afzkl.development.colorpickerview.demo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
